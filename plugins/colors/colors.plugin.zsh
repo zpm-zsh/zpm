9c10f266efbd3de85165212ed7b415979ccb5b1d
Grigorii Horos <horosgrisa@gmail.com>
2015-03-01 16:31:27 +0200

    modified:   plugins/colors/colors.plugin.zsh

--- plugins/colors/colors.plugin.zsh
+++ plugins/colors/colors.plugin.zsh
@@ -4,12 +4,12 @@ export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='4;31'
 export PAGER=${PAGER:-"less"}
 
-export LESS_TERMCAP_mb=$'\E[01;31m'
-export LESS_TERMCAP_md=$'\E[01;38;5;74m'
+export LESS_TERMCAP_mb=$'\E[00;32m' 
+export LESS_TERMCAP_md=$'\E[00;34m' 
 export LESS_TERMCAP_me=$'\E[0m'
-export LESS_TERMCAP_so=$'\E[38;5;246m'
+export LESS_TERMCAP_so=$'\E[00;33m'
 export LESS_TERMCAP_se=$'\E[0m'
-export LESS_TERMCAP_us=$'\E[04;38;5;146m'
+export LESS_TERMCAP_us=$'\E[00;32m'
 export LESS_TERMCAP_ue=$'\E[0m'
 export LESS='-R -M'
            
