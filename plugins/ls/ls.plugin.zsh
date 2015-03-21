d79d994fcea1b117ec38a3f8242b8aa92b913788
Grigorii Horos <horosgrisa@gmail.com>
2015-03-21 18:41:28 +0200

    modified:   plugins/ls/ls.plugin.zsh

--- plugins/ls/ls.plugin.zsh
+++ plugins/ls/ls.plugin.zsh
@@ -3,19 +3,19 @@
 if ls --version >/dev/null 2>&1 ; then
     if [[ $COLORS == "true" ]]; then
         if hash grc 2>/dev/null; then
-            alias ll="grc --config=${0:a:h}/conf.other ls -lFh --color  --group-directories-first --time-style=+%d/%m/%g\ %H:%M"
-            alias lsd="grc --config=${0:a:h}/conf.other ls -lFh --color  --group-directories-first --time-style=+%d/%m/%g\ %H:%M -d *(-/DN)"
-            alias ,="grc --config=${0:a:h}/conf.other ls -lFh --color  --group-directories-first --time-style=+%d/%m/%g\ %H:%M"
+            alias ll="grc --config=${0:a:h}/conf.other ls -lFh --color  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M"
+            alias lsd="grc --config=${0:a:h}/conf.other ls -lFh --color  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M -d *(-/DN)"
+            alias ,="grc --config=${0:a:h}/conf.other ls -lFh --color  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M"
         else
-            alias ll="ls -lFh --color  --group-directories-first --time-style=+%d/%m/%g\ %H:%M"
-            alias lsd="ls -lFh --color  --group-directories-first --time-style=+%d/%m/%g\ %H:%M -d *(-/DN)"
-            alias ,="ls -lFh --color  --group-directories-first --time-style=+%d/%m/%g\ %H:%M"
+            alias ll="ls -lFh --color  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M"
+            alias lsd="ls -lFh --color  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M -d *(-/DN)"
+            alias ,="ls -lFh --color  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M"
         fi
         alias ls='ls -CFlxBh --color --group-directories-first'
     else
-        alias ll="ls -lFh --color=never  --group-directories-first --time-style=+%d/%m/%g\ %H:%M"
-        alias lsd="ls -lFh --color=never  --group-directories-first --time-style=+%d/%m/%g\ %H:%M -d *(-/DN)"
-        alias ,="ls -lFh --color=never  --group-directories-first --time-style=+%d/%m/%g\ %H:%M"
+        alias ll="ls -lFh --color=never  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M"
+        alias lsd="ls -lFh --color=never  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M -d *(-/DN)"
+        alias ,="ls -lFh --color=never  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M"
     fi
 
     alias l="ls"
@@ -24,19 +24,19 @@ else
     if hash gls 2>/dev/null; then
         if [[ $COLORS == "true" ]]; then
             if hash grc 2>/dev/null; then
-                alias ll="grc --config=${0:a:h}/conf.other gls -lFh --color  --group-directories-first --time-style=+%d/%m/%g\ %H:%M"
-                alias lsd="grc --config=${0:a:h}/conf.other gls -lFh --color  --group-directories-first --time-style=+%d/%m/%g\ %H:%M -d *(-/DN)"
-                alias ,="grc --config=${0:a:h}/conf.other gls -lFh --color  --group-directories-first --time-style=+%d/%m/%g\ %H:%M"
+                alias ll="grc --config=${0:a:h}/conf.other gls -lFh --color  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M"
+                alias lsd="grc --config=${0:a:h}/conf.other gls -lFh --color  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M -d *(-/DN)"
+                alias ,="grc --config=${0:a:h}/conf.other gls -lFh --color  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M"
             else
-                alias ll="gls -lFh --color  --group-directories-first --time-style=+%d/%m/%g\ %H:%M"
-                alias lsd="gls -lFh --color  --group-directories-first --time-style=+%d/%m/%g\ %H:%M -d *(-/DN)"
-                alias ,="gls -lFh --color  --group-directories-first --time-style=+%d/%m/%g\ %H:%M"
+                alias ll="gls -lFh --color  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M"
+                alias lsd="gls -lFh --color  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M -d *(-/DN)"
+                alias ,="gls -lFh --color  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M"
             fi
             alias ls='gls -CFlxBh --color --group-directories-first'
         else
-            alias ll="gls -lFh --color=never  --group-directories-first --time-style=+%d/%m/%g\ %H:%M"
-            alias lsd="gls -lFh --color=never  --group-directories-first --time-style=+%d/%m/%g\ %H:%M -d *(-/DN)"
-            alias ,="gls -lFh --color=never  --group-directories-first --time-style=+%d/%m/%g\ %H:%M"
+            alias ll="gls -lFh --color=never  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M"
+            alias lsd="gls -lFh --color=never  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M -d *(-/DN)"
+            alias ,="gls -lFh --color=never  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M"
         fi
         alias l="gls"
         alias la='gls -A'
