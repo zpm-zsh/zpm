c90a07b4655b9b09057dd2afee7830b21e584ab2
Grigorii Horos <horosgrisa@gmail.com>
2015-03-21 09:48:30 +0200

    modified:   plugins/path/path.plugin.zsh deleted:    plugins/tor/bin/multi-tor

--- plugins/path/path.plugin.zsh
+++ plugins/path/path.plugin.zsh
@@ -4,12 +4,12 @@ CURRENT_PATH_PREFIX=${CURRENT_PATH_PREFIX:-""}
 CURRENT_PATH_SUFIX=${CURRENT_PATH_SUFIX:-""}
 path() {
     echo $PATH | tr ":" "\n" | \
-        awk "{ sub(\"/usr\",   \"$fg_no_bold[green]/usr$reset_color\"); \
-            sub(\"/bin\",   \"$fg_no_bold[blue]/bin$reset_color\"); \
-            sub(\"/games\",   \"$fg_no_bold[red]/games$reset_color\"); \
-            sub(\"/opt\",   \"$fg_no_bold[cyan]/opt$reset_color\"); \
-            sub(\"/sbin\",  \"$fg_no_bold[magenta]/sbin$reset_color\"); \
-            sub(\"/local\", \"$fg_no_bold[yellow]/local$reset_color\"); \
+        awk "{ sub(\"/usr\",\"$fg_no_bold[green]/usr$reset_color\"); \
+            sub(\"/bin\",\"$fg_no_bold[blue]/bin$reset_color\"); \
+            sub(\"/games\",\"$fg_no_bold[red]/games$reset_color\"); \
+            sub(\"/opt\",\"$fg_no_bold[cyan]/opt$reset_color\"); \
+            sub(\"/sbin\",\"$fg_no_bold[magenta]/sbin$reset_color\"); \
+            sub(\"/local\",\"$fg_no_bold[yellow]/local$reset_color\"); \
             print }"
 }
 
