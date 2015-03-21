4fa6983b4f821e5897dfc1fbb4c89f18823fc47e
Grigorii Horos <horosgrisa@gmail.com>
2015-03-21 18:19:49 +0200

    modified:   plugins/oh-my-zsh-wrapper/oh-my-zsh-wrapper.plugin.zsh

--- plugins/oh-my-zsh-wrapper/oh-my-zsh-wrapper.plugin.zsh
+++ plugins/oh-my-zsh-wrapper/oh-my-zsh-wrapper.plugin.zsh
@@ -1,5 +1,10 @@
 #!/usr/bin/env zsh
 
+if [[ ! -d ~/.oh-my-zsh ]]; then
+        git clone --recursive https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
+fi
+
+export ZSH=$HOME/.oh-my-zsh
 
 for plugin ($plugins); do
     if [ -f ~/.oh-my-zsh/plugins/$plugin/$plugin.plugin.zsh ]; then
@@ -10,3 +15,5 @@ done
 for plugin ($plugins); do
     fpath=(~/.oh-my-zsh/plugins/$plugin $fpath)
 done
+
+unset ZSH
