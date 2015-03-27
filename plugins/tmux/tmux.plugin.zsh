4d96e5ab14e4b775108710f4e39b388bbe672512
Grigorii Horos <horosgrisa@gmail.com>
2015-03-28 01:48:43 +0200

    Bugfix: TMUX

--- plugins/tmux/tmux.plugin.zsh
+++ plugins/tmux/tmux.plugin.zsh
@@ -1,11 +1,11 @@
 alias tmux="TERM=xterm-256color tmux -2 attach || TERM=xterm-256color tmux -2 -f${0:a:h}/tmux.conf new"
 
 if [[ -z $TMUX_AUTOSTART  && -n "$SSH_CONNECTION" ]]; then
-    export TMUX_AUTOSTART="true"
+    TMUX_AUTOSTART="true"
 fi
 
 
-if [[ "$TMUX_AUTOSTART" == "true" && ! -n "$TMUX" ]]
+if [[ "$TMUX_AUTOSTART" == "true" && -z "$TMUX" ]]
 then
     tmux
     exit 0
