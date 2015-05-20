f71d78f9910c00050cd250bfb6c70f35f485d1d0
Grigorii Horos <horosgrisa@gmail.com>
2015-05-21 00:06:57 +0300

    Add upgrading hook for nvm modified:   plugins/nvm/nvm.plugin.zsh

--- plugins/nvm/nvm.plugin.zsh
+++ plugins/nvm/nvm.plugin.zsh
@@ -1,3 +1,9 @@
 # The addition 'nvm install' attempts in ~/.profile
 
 [[ -s ~/.nvm/nvm.sh ]] && . ~/.nvm/nvm.sh
+
+function _nvm-update-hook(){
+	echo ">> Updating hook: nvm"
+    git --git-dir="$HOME/.nvm/.git/" --work-tree="$HOME/.nvm/" pull
+
+}
