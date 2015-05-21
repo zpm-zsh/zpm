ad97fe5b64ac34a1fa4ba96460bb478bc4168e44
Grigorii Horos <horosgrisa@gmail.com>
2015-05-21 21:42:59 +0300

    modified:   plugins/nvm/nvm.plugin.zsh

--- plugins/nvm/nvm.plugin.zsh
+++ plugins/nvm/nvm.plugin.zsh
@@ -3,7 +3,8 @@
 [[ -s ~/.nvm/nvm.sh ]] && . ~/.nvm/nvm.sh
 
 function _nvm-update-hook(){
-	echo ">> Updating hook: nvm"
-    git --git-dir="$HOME/.nvm/.git/" --work-tree="$HOME/.nvm/" pull
-
+	if [[ -d "$HOME/.nvm/" ]]; then
+	    echo ">> Updating hook: nvm"
+        git --git-dir="$HOME/.nvm/.git/" --work-tree="$HOME/.nvm/" pull
+    fi
 }
