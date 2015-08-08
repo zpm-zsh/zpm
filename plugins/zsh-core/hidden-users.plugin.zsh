3197e35f6ce76310d939eb087f2cca27c3706515
Grigorii Horos <horosgrisa@gmail.com>
2015-08-09 01:22:31 +0300

    Update list of hidden users

--- plugins/zsh-core/hidden-users.plugin.zsh
+++ plugins/zsh-core/hidden-users.plugin.zsh
@@ -1,10 +1,12 @@
 #!/usr/bin/env zsh
 
 zstyle ':completion:*:*:*:users' ignored-patterns \
+games gnats \
 polkitd avahi colord dbus nobody rtkit uuidd bin daemon sddm \
 http git ntp ftp mysql mail dnsmasq \
 systemd systemd-journal-gateway systemd-journal-remote systemd-journal-upload \
 systemd-timesync systemd-resolve systemd-network systemd-bus-proxy
 
 zstyle ':completion:*:*:*:groups' ignored-patterns \
-wheel
+wheel adm audio cdrom dip floppy \
+netdev plugdev sudo video
