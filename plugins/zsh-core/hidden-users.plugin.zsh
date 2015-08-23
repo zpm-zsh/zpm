e2b6a05621aa868e44384117b28905a361fe4b66
Grigorii Horos <horosgrisa@gmail.com>
2015-08-23 20:18:50 +0300

    Fiz hidden users/groups

--- plugins/zsh-core/hidden-users.plugin.zsh
+++ plugins/zsh-core/hidden-users.plugin.zsh
@@ -1,12 +1,13 @@
 #!/usr/bin/env zsh
 
 zstyle ':completion:*:*:*:users' ignored-patterns \
-games gnats \
-polkitd avahi colord dbus nobody rtkit uuidd bin daemon sddm \
-http git ntp ftp mysql mail dnsmasq \
-systemd systemd-journal-gateway systemd-journal-remote systemd-journal-upload \
-systemd-timesync systemd-resolve systemd-network systemd-bus-proxy
+bin daemon mail ftp http uuidd dbus nobody \
+git polkitd dnsmasq sddm avahi rtkit colord \
+systemd-journal-gateway systemd-timesync systemd-network systemd-bus-proxy \
+systemd-resolve systemd-journal-remote systemd-journal-upload 
 
 zstyle ':completion:*:*:*:groups' ignored-patterns \
-wheel adm audio cdrom dip floppy \
-netdev plugdev sudo video
+bin daemon sys adm tty disk lp mem kmem wheel ftp mail uucp log \
+utmp locate rfkill smmsp proc http games lock uuidd dbus network video audio optical floppy \
+storage scanner input power nobody users git polkitd dnsmasq sddm avahi rtkit mysql colord docker \
+systemd-journal systemd-journal-gateway systemd-timesync systemd-network systemd-bus-proxy systemd-resolve systemd-journal-remote systemd-journal-upload
