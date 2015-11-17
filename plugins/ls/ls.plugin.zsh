e61ae975f2c1599b9692f226e4d8275ab79172c2
Grigorii Horos <horosgrisa@gmail.com>
2015-11-17 11:56:50 +0200

    Fix ISO time

--- plugins/ls/ls.plugin.zsh
+++ plugins/ls/ls.plugin.zsh
@@ -3,19 +3,19 @@
 if ls --version >/dev/null 2>&1 ; then
     if [[ $COLORS == "true" ]]; then
         if hash grc 2>/dev/null; then
-            alias ll="grc --config=${${(%):-%x}:a:h}/conf.other ls -lFh --color  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M"
-            alias lsd="grc --config=${${(%):-%x}:a:h}/conf.other ls -lFh --color  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M -d *(-/DN)"
+            alias ll="grc --config=${${(%):-%x}:a:h}/conf.other ls -lFh --color  --group-directories-first --time-style=+%Y-%m-%d\ %H:%M"
+            alias lsd="grc --config=${${(%):-%x}:a:h}/conf.other ls -lFh --color  --group-directories-first --time-style=+%Y-%m-%d\ %H:%M -d *(-/DN)"
         else
-            alias ll="ls -lFh --color  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M"
-            alias lsd="ls -lFh --color  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M -d *(-/DN)"
+            alias ll="ls -lFh --color  --group-directories-first --time-style=+%Y-%m-%d\ %H:%M"
+            alias lsd="ls -lFh --color  --group-directories-first --time-style=+%Y-%m-%d\ %H:%M -d *(-/DN)"
         fi
         alias ls='ls -CFlxBh --color --group-directories-first'
         alias l="ls -CFlxBh --color --group-directories-first"
         alias la='ls -CFlxBh --color --group-directories-first -A'
 
     else
-        alias ll="ls -lFh --color=never  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M"
-        alias lsd="ls -lFh --color=never  --group-directories-first --time-style=+%Y/%m/%d\ %H:%M -d *(-/DN)"
+        alias ll="ls -lFh --color=never  --group-directories-first --time-style=+%Y-%m-%d\ %H:%M"
+        alias lsd="ls -lFh --color=never  --group-directories-first --time-style=+%Y-%m-%d\ %H:%M -d *(-/DN)"
         alias ls='ls -CFlxBh --color=never --group-directories-first'
         alias l="ls -CFlxBh --color=never --group-directories-first"
         alias la='ls -CFlxBh --color=never --group-directories-first -A'   
