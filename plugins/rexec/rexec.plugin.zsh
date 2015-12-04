9619fe05918b0da0fa967a161d9c93dfc1c2be26
Grigorii Horos <horosgrisa@gmail.com>
2015-12-04 13:35:55 +0200

    Changes in plugin rexec

--- plugins/rexec/rexec.plugin.zsh
+++ plugins/rexec/rexec.plugin.zsh
@@ -1,18 +1,30 @@
 #!/usr/bin/env zsh
 
 function s(){
+
     number=`wc -l</proc/mounts`
     for line in {1..$number}; do
         mount=`sed $line'!d' /proc/mounts`
-        if [[ $mount == *sshfs* ]]; then
-
-            host=`sed $line'!d' /proc/mounts | awk -F  ":" '{print $1}'`
-            text=`sed $line'!d' /proc/mounts | awk '{print $2}'`
-            text="echo \$\'"$(echo $text|sed -e 's|\\|\\\\|g')"\'"
-            text=$(eval "echo $(eval $text)")
-            read text < <(echo $text)
+        if [[ $mount == *fuse.sshfs* ]]; then
+            host=$(echo $mount | awk '{print $1}' | awk -F':' '{print $1}')
+            remotepath=$(echo $mount | awk '{print $1}' | awk -F':' '{print $2}')
+            localpath=$(echo $mount | awk '{print $2}')
+            localsuffix=${PWD#$localpath}
+            remotepath=$remotepath$localsuffix
+            script="""
+            export LS_COLORS=\"$LS_COLORS\"
+            export LANG=\"$LANG\"
+            export LANGUAGE=\"$LANGUAGE\"
+            export EDITOR=\"$EDITOR\"
+            export PAGER=\"$PAGER\"
+            export LESS=\"$LESS\"
+            export LESSOPEN=\"$LESSOPEN\"
+            export GREP_COLOR=\"$GREP_COLOR\"
+            cd $remotepath
+            $@
+            """
             if [[ `pwd` == $text* ]]; then
-                ssh "$host" "$@"
+                ssh $host $script
             fi
         fi
     done
