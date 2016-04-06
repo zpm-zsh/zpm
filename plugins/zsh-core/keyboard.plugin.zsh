0837c3ccea1fa3c2bd5a622b768774d191faaca2
Grigorii Horos <horosgrisa@gmail.com>
2016-04-06 17:07:25 +0300

    Fix keyboard codes in some system

--- plugins/zsh-core/keyboard.plugin.zsh
+++ plugins/zsh-core/keyboard.plugin.zsh
@@ -2,18 +2,41 @@
 
 zmodload zsh/complist
  
-bindkey '^A' beginning-of-line
-bindkey '^E' end-of-line
-bindkey '^[[1;5D' backward-word
-bindkey '^[OD' backward-word
-bindkey '^[[1;5C' forward-word
-bindkey '^[OC' forward-word
-bindkey '^[[3~' delete-char
-bindkey '^[[2~' overwrite-mode
-bindkey '^[[1;5A' history-beginning-search-backward
-bindkey '^[OA' history-beginning-search-backward
-bindkey '^[[1;5B' history-beginning-search-forward
-bindkey '^[OB' history-beginning-search-forward
+typeset -A key
+
+key[Home]=${terminfo[khome]}
+key[End]=${terminfo[kend]}
+key[Insert]=${terminfo[kich1]}
+key[Delete]=${terminfo[kdch1]}
+key[Up]=${terminfo[kcuu1]}
+key[Down]=${terminfo[kcud1]}
+key[Left]=${terminfo[kcub1]}
+key[Right]=${terminfo[kcuf1]}
+key[PageUp]=${terminfo[kpp]}
+key[PageDown]=${terminfo[knp]}
+
+# setup key accordingly
+[[ -n "${key[Home]}"    ]]  && bindkey  "${key[Home]}"    beginning-of-line
+[[ -n "${key[End]}"     ]]  && bindkey  "${key[End]}"     end-of-line
+[[ -n "${key[Insert]}"  ]]  && bindkey  "${key[Insert]}"  overwrite-mode
+[[ -n "${key[Delete]}"  ]]  && bindkey  "${key[Delete]}"  delete-char
+[[ -n "${key[Up]}"      ]]  && bindkey  "${key[Up]}"      up-line-or-history
+[[ -n "${key[Down]}"    ]]  && bindkey  "${key[Down]}"    down-line-or-history
+[[ -n "${key[Left]}"    ]]  && bindkey  "${key[Left]}"    backward-char
+[[ -n "${key[Right]}"   ]]  && bindkey  "${key[Right]}"   forward-char
+
+# Finally, make sure the terminal is in application mode, when zle is
+# active. Only then are the values from $terminfo valid.
+function zle-line-init () {
+    echoti smkx
+}
+function zle-line-finish () {
+    echoti rmkx
+}
+zle -N zle-line-init
+zle -N zle-line-finish  
+
+
 
 bindkey '^K' clear-screen
 bindkey "^W" backward-kill-word
