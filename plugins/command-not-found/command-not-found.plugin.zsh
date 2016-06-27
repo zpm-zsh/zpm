46cf9a214c9e9ae5379f21abb1730a90801e15c9
Grigorii Horos <horosgrisa@gmail.com>
2016-06-27 14:01:23 +0300

    Add left prompt

--- plugins/command-not-found/command-not-found.plugin.zsh
+++ plugins/command-not-found/command-not-found.plugin.zsh
@@ -1,5 +1,5 @@
 if [[ -f /usr/share/doc/pkgfile/command-not-found.zsh ]]; then
     source /usr/share/doc/pkgfile/command-not-found.zsh
-elif [[ -f /etc/zsh_command_not_found ]]
+elif [[ -f /etc/zsh_command_not_found ]]; then
     source /etc/zsh_command_not_found
 fi
