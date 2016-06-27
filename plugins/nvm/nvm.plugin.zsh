7edfd92669d012e356647e53525b031f9e02335a
Grigorii Horos <horosgrisa@gmail.com>
2016-06-27 14:33:59 +0300

    Fix nvm error

--- plugins/nvm/nvm.plugin.zsh
+++ plugins/nvm/nvm.plugin.zsh
@@ -3,16 +3,16 @@
 [[ -s ~/.nvm/nvm.sh ]] && . ~/.nvm/nvm.sh
 
 function _nvm-upgrade(){
-	if [[ -d "$HOME/.nvm/" ]]; then
-	    echo ">> Updating hook: nvm"
-        git --git-dir="$HOME/.nvm/.git/" --work-tree="$HOME/.nvm/" pull
-    fi
+  if [[ -d "$HOME/.nvm/" ]]; then
+    echo ">> Updating hook: nvm"
+      git --git-dir="$HOME/.nvm/.git/" --work-tree="$HOME/.nvm/" pull
+  fi
 }
 
 if ! hash nvm 2>/dev/null; then
-    function nvm-install(){
-        git clone https://github.com/creationix/nvm ~/.nvm
-				[[ -s ~/.nvm/nvm.sh ]] && . ~/.nvm/nvm.sh
-				nvm alias default system
-    }
+  function nvm-install(){
+    git clone https://github.com/creationix/nvm ~/.nvm
+    [[ -s ~/.nvm/nvm.sh ]] && . ~/.nvm/nvm.sh
+    nvm alias default system
+  }
 fi
