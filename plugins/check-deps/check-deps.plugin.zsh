9fca605f39ca74521128029eeaeb3b7e3739d699
Grigorii Horos <horosgrisa@gmail.com>
2016-06-30 13:41:19 +0300

    Add Debian dependency check

--- plugins/check-deps/check-deps.plugin.zsh
+++ plugins/check-deps/check-deps.plugin.zsh
@@ -14,10 +14,10 @@ function Check-Deps(){
   fi
 
   if hash dpkg 2>/dev/null; then
-    DEPENDENCES_DEBIAN_MISSING=()
-    local DEB_PACKAGES=$(dpkg --list| awk '{print $2}'|awk -F':' '{print $1}')
+    local DEPENDENCES_DEBIAN_MISSING=()
+    _DEB_PACKAGES=$(dpkg --list| awk '{print $2}'|awk -F':' '{print $1}')
     for i ($DEPENDENCES_DEBIAN); do
-      if [[ ! $(echo $DEPENDENCES_DEBIAN | grep -q $i ) ]]; then
+      if [[ ! $(echo $_DEB_PACKAGES | grep -q $i ) ]]; then
         if [[ ! ${DEPENDENCES_DEBIAN_MISSING[(r)$i]} == $i ]] && DEPENDENCES_DEBIAN_MISSING+=( $i )
       fi
     done
@@ -27,7 +27,7 @@ function Check-Deps(){
   fi
 
   if hash npm 2>/dev/null; then
-    DEPENDENCES_NPM_MISSING=()
+    local DEPENDENCES_NPM_MISSING=()
     local NPM_PATH="$( npm config get prefix )/lib/node_modules"
     for i ($DEPENDENCES_NPM); do
       if [[ ! -d "$NPM_PATH/$i"  ]] ; then
