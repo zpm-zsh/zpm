6a2ad128ebec44d5c9333d33d3a0f517b08f0287
Grigorii Horos <horosgrisa@gmail.com>
2016-06-30 13:35:22 +0300

    Fix

--- plugins/check-deps/check-deps.plugin.zsh
+++ plugins/check-deps/check-deps.plugin.zsh
@@ -15,7 +15,7 @@ function Check-Deps(){
 
   if hash dpkg 2>/dev/null; then
     DEPENDENCES_DEBIAN_MISSING=()
-    DEB_PACKAGES="dpkg --list| awk '{print $2}'|awk -F':' '{print $1}'"
+    DEB_PACKAGES=$(dpkg --list| awk '{print $2}'|awk -F':' '{print $1}')
     for i ($DEPENDENCES_DEBIAN); do
       if ! (echo $DEPENDENCES_DEBIAN | grep -q $i ) ; then
         if [[ ! ${DEPENDENCES_DEBIAN_MISSING[(r)$i]} == $i ]] && DEPENDENCES_DEBIAN_MISSING+=( $i )
