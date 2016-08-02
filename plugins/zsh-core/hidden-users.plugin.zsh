f5e7c3ea198066f0e1f1dd4f59e383bba61b9ae5
Grigorii Horos <horosgrisa@gmail.com>
2016-08-02 14:41:39 +0300

    Add hidden users

--- plugins/zsh-core/hidden-users.plugin.zsh
+++ plugins/zsh-core/hidden-users.plugin.zsh
@@ -9,6 +9,9 @@ named netdump news nfsnobody nobody nscd ntp nut nx openvpn \
 operator pcap postfix postgres privoxy pulse pvm quagga radvd \
 rpc rpcuser rpm shutdown squid sshd sync uucp vcsa xfs \
 polkitd rtkit nbd dnsmasq mpd uuidd sddm usbmux colord git http \
+lxc-dnsmasq avahi-autoipd nm-openvpn solr kernoops lightdm \
+man geoclue messagebus saned gnats usermetrics hplip sys \
+backup dhcpd irc syslog proxy list www-data \
 '_*' 'systemd-*'
 
 zstyle '*' single-ignored show
@@ -18,4 +21,5 @@ zstyle ':completion:*:*:*:groups' ignored-patterns \
 bin daemon sys adm tty disk lp mem kmem wheel ftp mail uucp log \
 utmp locate rfkill smmsp proc http games lock uuidd dbus network video audio optical floppy \
 storage scanner input power nobody users git polkitd dnsmasq sddm avahi rtkit mysql colord docker \
+cdrom dip lpadmin plugdev sambashare sudo \
 '_*' 'systemd-*'
