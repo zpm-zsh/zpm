98629f5756107786c8ff98c9e94bb547205446da
Grigorii Horos <horosgrisa@gmail.com>
2016-11-26 17:28:11 +0200

    Fix title & noempty

--- plugins/noempty/noempty.plugin.zsh
+++ plugins/noempty/noempty.plugin.zsh
@@ -9,6 +9,15 @@ magic-enter () {
 }
 
 zle -N magic-enter
-
 bindkey "^M" magic-enter
 
+function _pre_noempty(){
+  stty intr \^P
+}
+
+function _post_noempty(){
+  stty intr \^C
+}
+
+precmd_functions+=(_pre_noempty)
+preexec_functions+=(_post_noempty)
--- plugins/title/title.plugin.zsh
+++ plugins/title/title.plugin.zsh
@@ -3,14 +3,18 @@
 shortpath="%2~"
 function _precmd_title(){
   local program=zsh
-  if [[ -z $TMUX ]]; then
+  if [[ ! -z $TMUX ]]; then
+    print -Pn $( eval "echo '\033k'$TITLE'\033\\'")
+  else
     print -Pn $( eval "echo '\e]0;'$TITLE'\a'")
   fi
 }
 
 function _title(){
   local program=$(echo "$1" | cut -d" " -f1)
-  if [[ -z $TMUX ]]; then
+  if [[ ! -z $TMUX ]]; then
+    print -Pn $( eval "echo '\033k'$TITLE'\033\\'")
+  else
     print -Pn $( eval "echo '\e]0;'$TITLE'\a'")
   fi
 }
