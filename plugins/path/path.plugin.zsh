4f6b7e7fa89f712fcd826818d9fa69b88e35112a
Grigorii Horos <horosgrisa@gmail.com>
2016-11-27 00:01:01 +0200

    Refractoring of tmux theme

--- plugins/path/path.plugin.zsh
+++ plugins/path/path.plugin.zsh
@@ -9,20 +9,18 @@ path() {
 
 p() {
   pa=$(pwd)
-  pa=${pa//\//$fg[magenta]\/$fg[cyan]}
+  pa=${pa//\//$fg[red]\/$fg[cyan]}
   pa=$fg[cyan]$pa
   echo $pa
 }
 
 _current_path() {
   if [[ -z "$TMUX" ]]; then
+    newPWD=$(print -Pn %3~)
     newHOME=$(echo $HOME | sed 's/\//\\\//g')
-    newPWD=$(echo $PWD| sed 's/^'$newHOME'/~/g')
-    if [ $(echo -n "$newPWD" | wc -c | tr -d " ") -gt 2 ]; then
-      newPWD=$(echo -n "$newPWD" | awk -F '/' '{print $(NF-1) "/" $(NF)}')
-    fi
+    newPWD=$(echo $newPWD| sed 's/^'$newHOME'/~/g')
     if [[ $COLORS == "true" ]]; then
-      newPWD=${newPWD//\//%{$fg[magenta]%}\/%{$fg[cyan]%}}
+      newPWD=${newPWD//\//%{$fg[red]%}\/%{$fg[cyan]%}}
       current_path="$CURRENT_PATH_PREFIX%{$fg[cyan]%}$newPWD$CURRENT_PATH_SUFIX%{$reset_color%}"
     else
       current_path="$CURRENT_PATH_PREFIX$newPWD$CURRENT_PATH_SUFIX"
