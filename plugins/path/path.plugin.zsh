9c7b5ebacfb7098ecd5d529beda58ac507f83cec
Grigorii Horos <horosgrisa@gmail.com>
2016-11-29 17:56:25 +0200

    Fixes

--- plugins/path/path.plugin.zsh
+++ plugins/path/path.plugin.zsh
@@ -15,16 +15,14 @@ p() {
 }
 
 _current_path() {
-  if [[ -z "$TMUX" ]]; then
-    newPWD=$(print -Pn %3~)
-    newHOME=$(echo $HOME | sed 's/\//\\\//g')
-    newPWD=$(echo $newPWD| sed 's/^'$newHOME'/~/g')
-    if [[ $COLORS == "true" ]]; then
-      newPWD=${newPWD//\//%{$fg[red]%}\/%{$fg[cyan]%}}
-      current_path="$CURRENT_PATH_PREFIX%{$fg[cyan]%}$newPWD$CURRENT_PATH_SUFIX%{$reset_color%}"
-    else
-      current_path="$CURRENT_PATH_PREFIX$newPWD$CURRENT_PATH_SUFIX"
-    fi
+  newPWD=$(print -Pn %3~)
+  newHOME=$(echo $HOME | sed 's/\//\\\//g')
+  newPWD=$(echo $newPWD| sed 's/^'$newHOME'/~/g')
+  if [[ $COLORS == "true" ]]; then
+    newPWD=${newPWD//\//%{$fg[red]%}\/%{$fg[cyan]%}}
+    current_path="$CURRENT_PATH_PREFIX%{$fg[cyan]%}$newPWD$CURRENT_PATH_SUFIX%{$reset_color%}"
+  else
+    current_path="$CURRENT_PATH_PREFIX$newPWD$CURRENT_PATH_SUFIX"
   fi
 }
 _current_path
