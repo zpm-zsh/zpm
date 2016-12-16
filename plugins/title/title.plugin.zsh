0d9d73680f51ad9e31658ede921d906f796f99f6
Grigorii Horos <horosgrisa@gmail.com>
2016-12-16 18:26:28 +0200

    Fix

--- plugins/title/title.plugin.zsh
+++ plugins/title/title.plugin.zsh
@@ -3,18 +3,14 @@
 shortpath="%2~"
 function _precmd_title(){
   local program=zsh
-  if [[ ! -z $TMUX ]]; then
-    print -Pn $( eval "echo '\033k'$TITLE'\033\\'")
-  else
+  if [[ -z $TMUX ]]; then
     print -Pn $( eval "echo '\e]0;'$TITLE'\a'")
   fi
 }
 
 function _title(){
   local program=$(echo "$1" | cut -d" " -f1)
-  if [[ ! -z $TMUX ]]; then
-    print -Pn $( eval "echo '\033k'$TITLE'\033\\'")
-  else
+  if [[ -z $TMUX ]]; then
     print -Pn $( eval "echo '\e]0;'$TITLE'\a'")
   fi
 }
