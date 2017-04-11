2529b51ee5b28a586adf0ca1e73f8765fe8e39de
Grigorii Horos <horosgrisa@gmail.com>
2017-04-11 22:57:21 +0300

    Add compilation

--- lib/compile.zsh
+++ lib/compile.zsh
@@ -2,7 +2,17 @@
 
 {
   zcompdump="${ZDOTDIR:-$HOME}/.zcompdump"
+
   if [[ -s "$zcompdump" && (! -s "${zcompdump}.zwc" || "$zcompdump" -nt "${zcompdump}.zwc") ]]; then
     zcompile "$zcompdump"
   fi
+
+  find ${_ZPM_DIR}/ -name "*.zsh" | while read file; do
+    zcompile $file
+  done
+
+  find ${_ZPM_PLUGIN_DIR}/ -name "*.zsh" | while read file; do
+    zcompile $file
+  done
+
 } &!
