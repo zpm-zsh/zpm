59468e36658513725932c337703d046283aa2e11
Grigorii Horos <horosgrisa@gmail.com>
2017-04-11 15:52:52 +0300

    remove unused

--- zshrc-example.zsh
+++ zshrc-example.zsh
@@ -9,17 +9,8 @@ fi
 
 Plug zsh-core human ls cd colors dircolors tmux
 Plug desktop node git readers ssh
-Plug noempty user_prompt path ps2 eol title
-### 3party plugins
-Plug zsh-users/zsh-completions horosgrisa/autoenv 
-Plug horosgrisa/mysql-colorize horosgrisa/zsh-dropbox
-Plug jocelynmallon/zshmarks voronkovich/gitignore.plugin.zsh 
-Plug psprint/history-search-multi-word
-Plug zdharma/fast-syntax-highlighting tarruda/zsh-autosuggestions horosgrisa/utils
-###
+Plug noempty user-prompt path ps2 eol title
 Plug termux check-deps oh-my-zsh-wrapper # Compatibility
-
-plugins=( emoji-clock ) # Plugins from oh-my-zsh
 
 # ZSH
 PROMPT='$user_prompt'
