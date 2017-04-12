3e5d0dfd437b6a5573bd3bcb2d7933004d65e39f
Grigorii Horos <horosgrisa@gmail.com>
2017-04-12 16:26:44 +0300

    Remove unused

--- plugins/human/human.plugin.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-alias df="$(whence df) -h"
-alias du="$(whence du) -h"
-alias free="$(whence free) -h"
-
-alias rm="$(whence rm) -v"
-alias mv="$(whence mv) -v"
-alias cp="$(whence cp) -v"
