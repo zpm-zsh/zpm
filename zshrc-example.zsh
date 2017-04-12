a060bdba8e50b8a1819362069d1e8d20ba97b898
Grigorii Horos <horosgrisa@gmail.com>
2017-04-12 15:09:37 +0300

    Refractoring

--- zshrc-example.zsh
+++ zshrc-example.zsh
@@ -3,20 +3,12 @@
 if [[ -f ~/.zpm/zpm.zsh ]]; then
   source ~/.zpm/zpm.zsh
 else
-  git clone --recursive https://github.com/horosgrisa/zpm ~/.zpm
+  git clone --recursive https://github.com/zpm-zsh/zpm ~/.zpm
   source ~/.zpm/zpm.zsh
 fi
 
-Plug zsh-core human ls cd dircolors tmux
-Plug noempty user-prompt 
-Plug node git path ps2 eol title
 Plug termux check-deps oh-my-zsh-wrapper # Compatibility
 
-# ZSH
-PROMPT='$user_prompt'
-RPROMPT='$node_version$gitprompt$current_path'
-PROMPT2='$ps2'
-PROMPT_EOL_MARK='$eol'
 
 [[ -f $HOME/.zshrc.custom ]] && source $HOME/.zshrc.custom || true
 
