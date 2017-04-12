641cbc560df28f7af1c9f20751cf394409d6e3dd
Grigorii Horos <horosgrisa@gmail.com>
2017-04-12 13:11:20 +0300

    Refractoring

--- lib/upgrade.zsh
+++ lib/upgrade.zsh
@@ -1,18 +1,45 @@
 #!/usr/bin/env zsh
 
-function ZPM-Upgrade(){
-  _Plugins_Upgrade=()
+function _Upgrade-core(){
 
-  if [[ -z $@ ]]; then
-    _Plugins_Upgrade+=( $_ZPM_GitHub_Plugins )
-    git --git-dir="$_ZPM_DIR/.git/" --work-tree="$_ZPM_DIR/" checkout "$_ZPM_DIR/" </dev/null >/dev/null 2>/dev/null 
-    git --git-dir="$_ZPM_DIR/.git/" --work-tree="$_ZPM_DIR/" pull </dev/null >/dev/null 2>/dev/null 
+  git --git-dir="$_ZPM_DIR/.git/" --work-tree="$_ZPM_DIR/" checkout "$_ZPM_DIR/" </dev/null >/dev/null 2>/dev/null 
+  git --git-dir="$_ZPM_DIR/.git/" --work-tree="$_ZPM_DIR/" pull </dev/null >/dev/null 2>/dev/null 
+  pid=$!
+
+  if [[ $COLORS=="true" ]]; then
+    echo -en "$fg[green]Updating ZPM  ${fg[yellow]}${spin[0]}"
+  else
+    echo -en "Updating ZPM  ${spin[0]}"
+  fi
+
+  while $( kill -0 $pid 2>/dev/null)
+  do
+    for i in "${spin[@]}"
+    do
+      echo -ne "\b$i"
+      sleep 0.1
+    done
+  done
+  echo -e "\b✓${reset_color}"
+
+  for i ($_ZPM_Core_Plugins); do
+    type _${i}-upgrade | grep -q "shell function" && _${i}-upgrade &
+  done
+
+}
+
+function _Upgrade-plugin(){
+
+  if [[ -d $_ZPM_PLUGIN_DIR/$i ]]; then
+
+    git --git-dir="$_ZPM_PLUGIN_DIR/$i/.git/" --work-tree="$_ZPM_PLUGIN_DIR/$i/" checkout "$_ZPM_PLUGIN_DIR/$i/" </dev/null >/dev/null 2>/dev/null 
+    git --git-dir="$_ZPM_PLUGIN_DIR/$i/.git/" --work-tree="$_ZPM_PLUGIN_DIR/$i/" pull</dev/null >/dev/null 2>/dev/null 
     pid=$!
 
     if [[ $COLORS=="true" ]]; then
-      echo -en "$fg[green]Updating ZPM  ${fg[yellow]}${spin[0]}"
+      echo -en "${fg[green]}Updating ${fg[cyan]} ${1} ${fg[green]} from ${fg[blue]}GitHub  ${fg[yellow]}${spin[0]}"
     else
-      echo -en "Updating ZPM  ${spin[0]}"
+      echo -en "Updating ${1} from GitHub  ${spin[0]}"
     fi
 
     while $( kill -0 $pid 2>/dev/null)
@@ -24,40 +51,26 @@ function ZPM-Upgrade(){
       done
     done
     echo -e "\b✓${reset_color}"
+  fi
+  type _${i}-upgrade | grep -q "shell function" && _$plugg-upgrade
 
-    echo "Run plugin hooks"
-    for plugg ($_ZPM_Core_Plugins); do
-      type _$plugg-upgrade | grep -q "shell function" && _$plugg-upgrade
-    done
-    find "$_ZPM_DIR" -name "*.zsh" | while read zsh_file; do
-      zcompile $zsh_file
-    done
+}
+
+
+function ZPM-Upgrade(){
+  _Plugins_Upgrade=()
+
+  if [[ -z $@ ]]; then
+    _Plugins_Upgrade+=( "ZPM" $_ZPM_GitHub_Plugins )
   else
     _Plugins_Upgrade+=($@)
   fi
 
   for i ($_Plugins_Upgrade); do
     if [[ "$i" == "ZPM" ]]; then
-      echo "> Updating ZPM"
-      git --git-dir="$_ZPM_DIR/.git/" --work-tree="$_ZPM_DIR/" checkout "$_ZPM_DIR/"
-      git --git-dir="$_ZPM_DIR/.git/" --work-tree="$_ZPM_DIR/" pull
-      echo "Run plugin hooks"
-      for plugg ($_ZPM_Core_Plugins); do
-        type _$plugg-upgrade | grep -q "shell function" && _$plugg-upgrade
-      done
-      find "$_ZPM_DIR" -name "*.zsh" | while read zsh_file; do
-        zcompile $zsh_file
-      done
+      _Upgrade-core
     else
-      if [[ -d $_ZPM_PLUGIN_DIR/$i ]]; then
-        echo "> Updating: $i"
-        git --git-dir="$_ZPM_PLUGIN_DIR/$i/.git/" --work-tree="$_ZPM_PLUGIN_DIR/$i/" checkout "$_ZPM_PLUGIN_DIR/$i/"
-        git --git-dir="$_ZPM_PLUGIN_DIR/$i/.git/" --work-tree="$_ZPM_PLUGIN_DIR/$i/" pull
-      fi
-      type _$i-upgrade | grep -q "shell function" && _$plugg-upgrade
-      find "$_ZPM_PLUGIN_DIR/$i" -name "*.zsh" | while read zsh_file; do
-        zcompile $zsh_file
-      done
+      _Upgrade-plugin $i
     fi
   done
 }
