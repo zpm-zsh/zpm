a068994ff3bb2ebf09a46bc785a2a1b01740bf7b
Grigorii Horos <horosgrisa@gmail.com>
2017-04-26 21:07:30 +0300

    Fix

--- plugins/ls/ls.plugin.zsh
+++ plugins/ls/ls.plugin.zsh
@@ -1,34 +1,55 @@
 #!/usr/bin/env zsh
 
-local LS="$(whence ls)"
+_LS_LS="$(whence ls)"
 
 if (( $+commands[gls] )); then
-  LS="$(whence gls)"
+  echo oo
+  _LS_LS="$(whence gls)"
 fi
 
-local IS_GNU_LS="false" # FIXME: Doesn't work on BSD* systems
+_LS_IS_GNU_LS="false" # FIXME: Doesn't work on BSD* systems
 if ls --version >/dev/null 2>&1 ; then
-  IS_GNU_LS="true"
+  _LS_IS_GNU_LS="true"
 fi
 
-local GRC=""
+_LS_GRC=""
 if (( $+commands[grc] )); then
-  GRC="grc --config=${${(%):-%x}:a:h}/conf.other "
+  _LS_GRC="grc --config=${${(%):-%x}:a:h}/conf.other "
 fi
 
-local COLOR=""
+_LS_COLOR=""
 if [[ "$COLORS" == "true" ]]; then
-  COLOR="--color"
+  _LS_COLOR="--color"
 else
-  COLOR="--color=never"
-  GRC=""
+  _LS_COLOR="--color=never"
+  _LS_GRC=""
 fi
 
-if [[ "$IS_GNU_LS" == "true" ]]; then
-    alias ll="$GRC$LS $COLOR -lFh  --group-directories-first --time-style=+%Y-%m-%d\ %H:%M"
-    alias lsd="$GRC$LS $COLOR -lFh  --group-directories-first --time-style=+%Y-%m-%d\ %H:%M -d *(-/DN)"
+if [[ "$_LS_IS_GNU_LS" == "true" ]]; then
 
-    alias ls='$LS $COLOR -CFlxBh --group-directories-first'
-    alias l="$LS $COLOR -CFlxBh --group-directories-first"
-    alias la='$LS $COLOR -CFlxBh --group-directories-first -A'
+  function ll(){
+    eval "$_LS_GRC $_LS_LS $_LS_COLOR -lFh --group-directories-first --time-style=+%Y-%m-%d\ %H:%M"
+  }
+  compdef ll=ls
+
+  function lsd(){
+    eval "$_LS_GRC $_LS_LS $_LS_COLOR -lFh  --group-directories-first --time-style=+%Y-%m-%d\ %H:%M -d *(-/DN)"
+  }
+  compdef lsd=ls
+
+  function la(){
+    eval "$_LS_LS $_LS_COLOR -CFlxBh --group-directories-first -A"
+  }
+  compdef la=ls
+
+  function l(){
+    eval "$_LS_LS $_LS_COLOR -CFlxBh --group-directories-first"
+  }
+  compdef l=ls
+
+  function ls(){
+    eval "$_LS_LS $_LS_COLOR -CFlxBh --group-directories-first"
+  }
+  compdef ls=ls
+  # alias ls='$LS $COLOR -CFlxBh --group-directories-first'
 fi
