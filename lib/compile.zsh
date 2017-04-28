239d2fbdd3ac176b9230c4d783387269606d72fe
Grigorii Horos <horosgrisa@gmail.com>
2017-04-28 07:28:27 +0300

    Fix zwc compilation

--- lib/compile.zsh
+++ lib/compile.zsh
@@ -4,15 +4,15 @@
   zcompdump="${ZDOTDIR:-$HOME}/.zcompdump"
 
   if [[ -s "$zcompdump" && (! -s "${zcompdump}.zwc" || "$zcompdump" -nt "${zcompdump}.zwc") ]]; then
-    zcompile "$zcompdump"
+    zcompile "$zcompdump" 1>/dev/null  2>/dev/nul
   fi
 
   find ${_ZPM_DIR}/ -type f -name "*.zsh" | while read file; do
-    zcompile $file
+    zcompile $file 1>/dev/null  2>/dev/nul
   done
 
   find ${_ZPM_PLUGIN_DIR}/ -type f -name "*.zsh" | while read file; do
-    zcompile $file
+    zcompile $file 1>/dev/null  2>/dev/nul
   done
 
 } &!
