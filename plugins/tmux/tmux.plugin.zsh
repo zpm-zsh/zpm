faedde197a068e01aea04313c298101b8888f97b
Grigorii Horos <horosgrisa@gmail.com>
2018-06-15 12:31:49 +0300

    Change tmux plugin

--- plugins/tmux/tmux.plugin.zsh
+++ plugins/tmux/tmux.plugin.zsh
@@ -1,19 +1,12 @@
 alias tmux="TERM=xterm-256color tmux -2 attach || TERM=xterm-256color tmux -2 -f${${(%):-%x}:a:h}/tmux.conf new"
 
 
-if [[ ! -d ~/.tmux/plugins/tpm ]]; then
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm </dev/null >/dev/null 2>/dev/null &!
-fi
-
 if [[ -z $TMUX_AUTOSTART  && -n "$SSH_CONNECTION" ]]; then
   if (( $+commands[tmux] )); then
     TMUX_AUTOSTART="true"
   fi
 fi
 
-function _tmux-upgrade(){
-  git --git-dir="$HOME/.tmux/plugins/tpm/.git" --work-tree="$HOME/.tmux/plugins/tpm" pull </dev/null >/dev/null 2>/dev/null 
-}
 
 function _tmux_autostart(){
   if [[ "$TMUX_AUTOSTART" == "true" && -z "$TMUX" ]]; then
@@ -38,5 +31,6 @@ function _tmux_motd(){
 if [[ $TMUX_AUTOSTART == true ]]; then
   _tmux_motd
 fi
+
 DEPENDENCES_ARCH+=( tmux )
 DEPENDENCES_DEBIAN+=( tmux )
