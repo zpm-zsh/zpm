067004569505df182f7c0d49eea5470a69ac162c
Grigorii Horos <horosgrisa@gmail.com>
2018-08-19 00:28:56 +0300

    Improve tmux plugin

--- plugins/tmux/tmux.plugin.zsh
+++ plugins/tmux/tmux.plugin.zsh
@@ -1,18 +1,19 @@
 alias tmux="TERM=xterm-256color tmux -2 attach || TERM=xterm-256color tmux -2 -f${${(%):-%x}:a:h}/tmux.conf new"
 
 
-if [[ -z $TMUX_AUTOSTART  && -n "$SSH_CONNECTION" ]]; then
-  if (( $+commands[tmux] )); then
+[[ ! -f ~/.tmux.conf ]] && touch ~/.tmux.conf
+
+TMUX_AUTOSTART="false"
+
+if (( $+commands[tmux] )); then
+  if [[ -n "$SSH_CONNECTION" ]]; then
     TMUX_AUTOSTART="true"
   fi
 fi
 
 
 function _tmux_autostart(){
-  if [[ ! -f ~/.tmux.conf ]]; then
-    touch ~/.tmux.conf
-  fi
-  
+
   if [[ "$TMUX_AUTOSTART" == "true" && -z "$TMUX" ]]; then
     TERM=xterm-256color \tmux -2 attach || TERM=xterm-256color \tmux -2 -f${${(%):-%x}:a:h}/tmux.conf new
     exit 0
@@ -22,19 +23,14 @@ function _tmux_autostart(){
 
 precmd_functions+=( _tmux_autostart )
 
-function _tmux_motd(){
-  if [[ $TMUX_MOTD != false && ! -z $TMUX  &&  $(\tmux list-windows | wc -l | tr -d ' ') == 1 ]] && ( \tmux list-windows | tr -d ' '|grep -q 1panes  ); then
-    if [[ "$OSTYPE" == linux* || "$OSTYPE" == freebsd*  ]]; then
-      _tmux_monitor
-      return 0
-    fi
+
+if [[ $TMUX_MOTD != false && ! -z $TMUX  &&  $(\tmux list-windows | wc -l | tr -d ' ') == 1 ]] && ( \tmux list-windows | tr -d ' '|grep -q 1panes  ); then
+  if [[ "$OSTYPE" == linux* || "$OSTYPE" == freebsd*  ]]; then
+    _tmux_monitor
+    return 0
   fi
-}
-
-
-if [[ $TMUX_AUTOSTART == true ]]; then
-  _tmux_motd
 fi
+
 
 DEPENDENCES_ARCH+=( tmux )
 DEPENDENCES_DEBIAN+=( tmux )
