75ed11c7d588e30c10fbb5854549a592319564d3
Grigorii Horos <horosgrisa@gmail.com>
2018-08-22 11:28:48 +0300

    changes in cd plugin

--- plugins/cd/cd.plugin.zsh
+++ plugins/cd/cd.plugin.zsh
@@ -1,6 +1,19 @@
 #!/usr/bin/env zsh
 
-setopt autocd
+setopt AUTO_CD              # Auto changes to a directory without typing cd.
+setopt AUTO_PUSHD           # Push the old directory onto the stack on cd.
+setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack.
+setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd.
+setopt PUSHD_TO_HOME        # Push to home directory when no argument is given.
+setopt CDABLE_VARS          # Change directory to a path stored in a variable.
+setopt MULTIOS              # Write to multiple descriptors.
+setopt EXTENDED_GLOB        # Use extended globbing syntax.
+unsetopt CLOBBER            # Do not overwrite existing files with > and >>.
+# Use >! and >>! to bypass.
+
+
+alias d='dirs -v'
+for index ({0..9}) alias "$index"="cd +${index}"; unset index
 
 alias tmp='cd $(mktemp -d)'
 alias cdo='cd -'
