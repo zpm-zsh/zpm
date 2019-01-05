1dc83a631ce77db5604ab49e3e9b4c05f43adc26
Grigorii Horos <horosgrisa@gmail.com>
2019-01-05 23:00:42 +0200

    Add hyperlinks for path

--- plugins/pr-cwd/pr-cwd.plugin.zsh
+++ plugins/pr-cwd/pr-cwd.plugin.zsh
@@ -5,19 +5,26 @@ CURRENT_PATH_SUFIX=${CURRENT_PATH_SUFIX:-""}
 
 _pr_cwd_HOME_=$(echo $HOME | sed 's/\//\\\//g')
 
+
 _pr_cwd() {
-  local newPWD=$(print -Pn %2~)
-  local newPWD=$(echo $newPWD| sed 's/^'$_pr_cwd_HOME_'/~/g')
+  
+  newPWD=$(print -Pn %2~)
+  newPWD=$(echo $newPWD| sed 's/^'$_pr_cwd_HOME_'/~/g')
+  
   local lockIcon=""
   if [[ ! -w . ]]; then
     lockIcon=" "
   fi
+  
   if [[ $CLICOLOR = 1 ]]; then
     newPWD=${newPWD//\//%{$fg_bold[red]%}\/%{$fg_bold[cyan]%}}
-    pr_cwd="$CURRENT_PATH_PREFIX%{$fg[red]%}$lockIcon%{$fg_bold[cyan]%}$newPWD$CURRENT_PATH_SUFIX%{$reset_color%}"
+    newPWD=$'%{\e]8;;file://'$PWD$'\e\\%}'$newPWD$'%{\e]8;;\e\\%}'
+    short_PWD="$CURRENT_PATH_PREFIX%{$fg[red]%}$lockIcon%{$fg_bold[cyan]%}$newPWD$CURRENT_PATH_SUFIX%{$reset_color%}"
   else
-    pr_cwd="$CURRENT_PATH_PREFIX$lockIcon$newPWD$CURRENT_PATH_SUFIX"
+    short_PWD="$CURRENT_PATH_PREFIX$lockIcon$newPWD$CURRENT_PATH_SUFIX"
   fi
+  
 }
+
 _pr_cwd
 chpwd_functions+=(_pr_cwd)
