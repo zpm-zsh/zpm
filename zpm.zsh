0270f0757905f6db35cef712612b92c8e4d4cfdc
Grigorii Horos <horosgrisa@gmail.com>
2019-04-19 23:55:20 +0300

    Add alias for update

--- zpm.zsh
+++ zpm.zsh
@@ -1,19 +1,14 @@
 #!/usr/bin/env zsh
 
 source ${${(%):-%x}:a:h}/lib/functions.zsh
-
 source ${${(%):-%x}:a:h}/lib/core.zsh
-
 source ${${(%):-%x}:a:h}/lib/install.zsh
-
 source ${${(%):-%x}:a:h}/lib/initialize.zsh
-
 source ${${(%):-%x}:a:h}/lib/upgrade.zsh
-
 source ${${(%):-%x}:a:h}/lib/compile.zsh
 
 function zpm(){
-  if [[ "$1" == 'up' || "$1" == 'upgrade' ]]; then
+  if [[ "$1" == 'u' || "$1" == 'up' || "$1" == 'upgrade' ]]; then
     shift
     _ZPM-upgrade $@
     return 0
