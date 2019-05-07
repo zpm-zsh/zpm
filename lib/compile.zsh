98e137af0ba8b40b401a733ec22072364545f51f
Grigorii Horos <horosgrisa@gmail.com>
2019-05-07 14:14:49 +0300

    Add nomatch option

--- lib/compile.zsh
+++ lib/compile.zsh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
 {
+  setopt +o nomatch
   autoload -Uz zrecompile
   zrecompile -q "~/.zshrc" "${HOME}/.zcompdump" ${_ZPM_DIR}/**/*.zsh ${_ZPM_PLUGIN_DIR}/**/*.zsh ${_ZPM_PLUGIN_DIR}/**/*.zsh-theme
 } &!
