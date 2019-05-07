8753975d4d31154fc1347d2ae19ea442977de871
Grigorii Horos <horosgrisa@gmail.com>
2019-05-07 14:21:46 +0300

    Ignore errors

--- lib/compile.zsh
+++ lib/compile.zsh
@@ -6,5 +6,5 @@
   zrecompile -q -p \
                     -R ~/.zshrc -- \
                     -M ~/.zcompdump -- \
-                    ~/.zshcache.zwc ${_ZPM_DIR}/**/*.zsh ${_ZPM_PLUGIN_DIR}/**/*.zsh ${_ZPM_PLUGIN_DIR}/**/*.zsh-theme 
+                    ~/.zshcache.zwc ${_ZPM_DIR}/**/*.zsh ${_ZPM_PLUGIN_DIR}/**/*.zsh ${_ZPM_PLUGIN_DIR}/**/*.zsh-theme 2>/dev/null
 } &!
