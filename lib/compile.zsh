757fe5d898f3984e439f3e77dfb86d846719ca5f
Grigorii Horos <horosgrisa@gmail.com>
2019-07-23 23:54:12 +0300

    Revert nomatch

--- lib/compile.zsh
+++ lib/compile.zsh
@@ -1,12 +1,13 @@
 #!/usr/bin/env zsh
 
 {
-  for file in  ~/.zshrc \
+setopt +o nomatch
+for file in  ~/.zshrc \
   ${_ZPM_DIR}/**/*.zsh \
   ${_ZPM_PLUGIN_DIR}/**/*.zsh \
   ${_ZPM_PLUGIN_DIR}/**/*.zsh-theme ; do
-    if [[ -f ${file} && ( ! -s ${file}.zwc || ${file} -nt ${file}.zwc) ]]; then
-      zcompile $file
-    fi
-  done
+  if [[ -f ${file} && ( ! -s ${file}.zwc || ${file} -nt ${file}.zwc) ]]; then
+    zcompile $file
+  fi
+done
 } &!
