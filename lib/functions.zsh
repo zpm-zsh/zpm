6969919d97eb8e03ef6b660f130d30d820063348
Grigorii Horos <horosgrisa@gmail.com>
2019-08-08 23:17:13 +0300

    Fix debug

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -1,10 +1,22 @@
 #!/usr/bin/env zsh
 
 function _ZPM-log() {
-  if [[ -n "$DEBUG" &&  "${1}" == "${DEBUG}"*  ]]; then
+  
+  if [[ -n "$DEBUG" &&  "${1}:" == "${DEBUG}:"*  ]]; then
+    
+    num=0
+    
+    for i in $(seq 1 ${#1}); do
+      num=$(( $num + $(LC_CTYPE=C printf '%d' "'${1[$i]})") ))
+    done
+    color=$(( $num % 6 + 1 ))
+    
+    echo -n "[1;3${color}m$1 [0m"
+    shift
     echo "$@"
   fi
 }
+
 
 function _ZPM-plugin-type() {
   if [[ ${1} == 'zpm' ]]; then
