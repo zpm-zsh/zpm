2f421fe5a06f6ac69814b50a8628523e21cb8f0f
Grigorii Horos <horosgrisa@gmail.com>
2019-10-31 17:37:49 +0200

    Fix check deps

--- lib/post.zsh
+++ lib/post.zsh
@@ -3,5 +3,6 @@
 post_fn(){
   echo 'PATH="'"${ZPM_PATH}"'${PATH}"' >> ~/.zsh.cache
   echo 'fpath=( '$ZPM_fpath' $fpath )' >> ~/.zsh.cache
+  echo '_ZPM_Plugins=( '$_ZPM_Plugins' )' >> ~/.zsh.cache
   echo 'zpm () {}' >> ~/.zsh.cache
 }
