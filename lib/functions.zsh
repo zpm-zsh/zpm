70c7d08521ade8cd65506a1d1856b14c8e8848e9
Grigorii Horos <horosgrisa@gmail.com>
2019-11-11 16:23:44 +0200

    Fix

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -26,7 +26,7 @@ function _ZPM-upgrade(){
   rm "$_ZPM_CACHE" 2>/dev/null
   
   if [[ -z $@ ]]; then
-    _Plugins_Upgrade+=( "zpm" $_ZPM_Plugins )
+    _Plugins_Upgrade+=( "zpm" $zsh_loaded_plugins )
   else
     _Plugins_Upgrade+=($@)
   fi
