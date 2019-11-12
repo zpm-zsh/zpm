c96748250b64e27dce9f14f3bdf52c2e895b6fb9
Grigorii Horos <horosgrisa@gmail.com>
2019-11-12 11:06:33 +0200

    Fix

--- lib/imperative.zsh
+++ lib/imperative.zsh
@@ -44,7 +44,7 @@ function _ZPM_Post_Initialization(){
   echo "typeset -a zsh_loaded_plugins=(${_ZPM_plugins_no_source})" >> "$_ZPM_CACHE"
   echo -n "typeset -A _ZPM_plugins_full=(" >> "$_ZPM_CACHE"
   for key val in ${(kv)_ZPM_plugins_full}; do
-    echo -n "${key} \"${val}\" " >> "$_ZPM_CACHE"
+    echo -n "\"${key}\" \"${val}\" " >> "$_ZPM_CACHE"
   done  
   echo ')' >> "$_ZPM_CACHE"
   echo >> "$_ZPM_CACHE"
