b69aaff14b4df01a7883d4b2c9e8af1b86302a0d
Grigorii Horos <horosgrisa@gmail.com>
2019-11-12 13:33:13 +0200

    Optimize

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -1,4 +1,39 @@
 #!/usr/bin/env zsh
+function zpm(){
+  if [[ "$1" == 'c' || "$1" == 'cl' || "$1" == 'clean' ]]; then
+    shift
+    _ZPM_clean
+    return 0
+  fi
+  
+  if [[ "$1" == 'u' || "$1" == 'up' || "$1" == 'upgrade' ]]; then
+    shift
+    _ZPM-upgrade "$@"
+    return 0
+  fi
+  
+  if [[ "$1" == 'load' ]]; then
+    shift
+  fi
+  
+  if [[ "$1" == 'load-if' || "$1" == 'if' ]]; then
+    if check-if "$2"; then
+      shift 2
+      zpm "$@"
+    fi
+    return 0
+  fi
+  
+  if [[ "$1" == 'load-if-not' || "$1" == 'if-not' ]]; then
+    if ! check-if "$2"; then
+      shift 2
+      zpm "$@"
+    fi
+    return 0
+  fi
+  
+  _ZPM-initialize-plugin "$@"
+}
 
 function _ZPM-log() {
   if [[ -n "$DEBUG" &&  "${1}:" == "${DEBUG}:"*  ]]; then
--- lib/imperative.zsh
+++ lib/imperative.zsh
@@ -1,41 +1,5 @@
 #!/usr/bin/env zsh
 
-function zpm(){
-  if [[ "$1" == 'c' || "$1" == 'cl' || "$1" == 'clean' ]]; then
-    shift
-    _ZPM_clean
-    return 0
-  fi
-  
-  if [[ "$1" == 'u' || "$1" == 'up' || "$1" == 'upgrade' ]]; then
-    shift
-    _ZPM-upgrade "$@"
-    return 0
-  fi
-  
-  if [[ "$1" == 'load' ]]; then
-    shift
-  fi
-  
-  if [[ "$1" == 'load-if' || "$1" == 'if' ]]; then
-    if check-if "$2"; then
-      shift 2
-      zpm "$@"
-    fi
-    return 0
-  fi
-  
-  if [[ "$1" == 'load-if-not' || "$1" == 'if-not' ]]; then
-    if ! check-if "$2"; then
-      shift 2
-      zpm "$@"
-    fi
-    return 0
-  fi
-  
-  _ZPM-initialize-plugin "$@"
-}
-
 function _ZPM_Post_Initialization(){
   
   echo > "$_ZPM_CACHE"
@@ -87,7 +51,6 @@ function _ZPM_Post_Initialization(){
   
   echo "source '${_ZPM_DIR}/lib/functions.zsh'" >> "$_ZPM_CACHE"
   echo "source '${_ZPM_DIR}/lib/initialize.zsh'" >> "$_ZPM_CACHE"
-  echo "source '${_ZPM_DIR}/lib/declarative.zsh'" >> "$_ZPM_CACHE"
   echo "source '${_ZPM_DIR}/lib/completion.zsh'" >> "$_ZPM_CACHE"
   
   echo >> "$_ZPM_CACHE"
