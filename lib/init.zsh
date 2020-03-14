cf69aed5e6c91e826cd01d965285fe0c48d74e2e
Grigorii Horos <horosgrisa@gmail.com>
2020-03-14 20:26:22 +0200

    add bin/ dir standard

--- lib/init.zsh
+++ lib/init.zsh
@@ -3,13 +3,14 @@
 # Zsh Plugin Standard compatibility
 # 0 – the plugin manager provides the ZERO parameter,
 # f - … supports the functions subdirectory,
+# f - … supports the bin subdirectory,
 # u - … the unload function,
 # U - … the @zsh-plugin-run-on-unload call,
 # p – … the @zsh-plugin-run-on-update call,
 # i – … the zsh_loaded_plugins activity indicator,
 # P – … the ZPFX global parameter,
 # s – … the PMSPEC global parameter itself (i.e.: should be always present).
-export PMSPEC="0fiPs"
+export PMSPEC="0fbiPs"
 
 export ZPFX="${HOME}/.local"
 
