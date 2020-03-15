e35e6f3f67b86026f231e426e5b85e67c0dc5f3e
Grigorii Horos <horosgrisa@gmail.com>
2020-03-15 21:07:37 +0200

    Optimize plugin

--- lib/zpm.zsh
+++ lib/zpm.zsh
@@ -43,18 +43,6 @@ function zpm() {
     return 0
   fi
 
-  if [[ "$1" == 'c' || "$1" == 'cl' || "$1" == 'clean' ]]; then
-    shift
-    _ZPM_clean
-    return 0
-  fi
-
-  if [[ "$1" == 'u' || "$1" == 'up' || "$1" == 'upgrade' ]]; then
-    shift
-    _ZPM_upgrade "$@"
-    return 0
-  fi
-
   if [[ "$1" == 'if' ]]; then
     if check-if "$2"; then
       shift 2
@@ -68,6 +56,18 @@ function zpm() {
       shift 2
       zpm "$@"
     fi
+    return 0
+  fi
+
+  if [[ "$1" == 'c' || "$1" == 'cl' || "$1" == 'clean' ]]; then
+    shift
+    _ZPM_clean
+    return 0
+  fi
+
+  if [[ "$1" == 'u' || "$1" == 'up' || "$1" == 'upgrade' ]]; then
+    shift
+    _ZPM_upgrade "$@"
     return 0
   fi
 
