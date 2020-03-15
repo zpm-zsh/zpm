63a1c0a7b46e71181bb4ef76ddcd267a2118a48f
Grigorii Horos <horosgrisa@gmail.com>
2020-03-15 21:07:57 +0200

    Optimize plugin

--- lib/imperative.zsh
+++ lib/imperative.zsh
@@ -2,7 +2,7 @@
 
 compinit -i -C -d "${ZPM_COMPDUMP}"
 
-zpm zpm-zsh/helpers zpm-zsh/colors zpm-zsh/background
+zpm load zpm-zsh/helpers zpm-zsh/colors zpm-zsh/background
 
 TMOUT=1
 add-zsh-hook background _ZPM_Background_Initialization
