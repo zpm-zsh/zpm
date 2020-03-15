878781a4944cbe4d72df69ad74ebfe6060e3d07f
Grigorii Horos <horosgrisa@gmail.com>
2020-03-15 22:54:39 +0200

    Optimize plugin

--- lib/imperative.zsh
+++ lib/imperative.zsh
@@ -6,3 +6,11 @@ zpm load zpm-zsh/helpers zpm-zsh/colors zpm-zsh/background
 
 TMOUT=1
 add-zsh-hook background _ZPM_Background_Initialization
+
+function source() {
+  if [[ -f "$1" && ( ! -s "$1.zwc" || "$1" -nt "$1.zwc") ]]; then;
+    zcompile "$1" 2>/dev/null
+  fi
+
+  builtin source "$1"
+}
