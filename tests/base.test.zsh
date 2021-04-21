e4f06c08940f64607b13770c8682f63266399d12
Grigorii Horos <horosgrisa@gmail.com>
2021-04-22 02:29:41 +0300

    Improve speed

--- tests/base.test.zsh
+++ tests/base.test.zsh
@@ -2,9 +2,9 @@
 # https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
-_ZPM_DIR=${0:h}
+ZSH_CUSTOM=${0:h}
 
 export ZPM_USE_CACHE=false
 
-source "${_ZPM_DIR}/../zpm.zsh"
+source "${_ZPM_DIR}/zpm.zsh"
 source "${0:h}/lib.zsh"
--- zpm.zsh
+++ zpm.zsh
@@ -11,26 +11,23 @@
 # s – … the PMSPEC global parameter itself (i.e.: should be always present).
 export PMSPEC="0fbPs"
 export ZPFX="${HOME}/.local"
-
-export _ZPM_DIR="${${(%):-%x}:h}"
-export _ZPM_PLUGIN_DIR="${_ZPM_DIR}/plugins"
+export ZSH_CUSTOM="${${(%):-%x}:h}"
 
 export ZSH_CACHE_DIR="${TMPDIR:-/tmp}/zsh-${UID:-user}"
 _ZPM_CACHE="${ZSH_CACHE_DIR}/zpm-cache.zsh"
 _ZPM_CACHE_ASYNC="${ZSH_CACHE_DIR}/zpm-cache-async.zsh"
 _ZPM_COMPDUMP="${ZSH_CACHE_DIR}/zcompdump"
 
-fpath=("${_ZPM_DIR}/functions" "${ZSH_CACHE_DIR}/functions" $fpath)
+fpath=("${ZSH_CUSTOM}/functions" "${ZSH_CACHE_DIR}/functions" $fpath)
 export PATH="${ZSH_CACHE_DIR}/bin:$PATH"
 typeset -aU path cdpath fpath manpath
 
-setopt caseglob
 autoload -Uz compinit
 zstyle ':completion:*:zpm:*' sort false
 
 if [[ -f "${_ZPM_CACHE}" ]]; then
   source "${_ZPM_CACHE}"
 else
-  eval "$(<${_ZPM_DIR}/lib/init.zsh)"
-  eval "$(<${_ZPM_DIR}/lib/imperative.zsh)"
+  eval "$(<${ZSH_CUSTOM}/lib/init.zsh)"
+  eval "$(<${ZSH_CUSTOM}/lib/imperative.zsh)"
 fi
