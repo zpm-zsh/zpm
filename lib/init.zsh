284867a5ee7d825ddd142621025fd523adffe485
Grigorii Horos <horosgrisa@gmail.com>
2021-11-06 18:28:39 +0200

    Add possibility to change parallel bin

--- lib/init.zsh
+++ lib/init.zsh
@@ -8,6 +8,49 @@ typeset -ag _ZPM_plugins_no_source
 typeset -Ag _ZPM_file_for_source
 typeset -Ag _ZPM_file_for_async_source
 
+typeset -g _zpm_parallel_format
+typeset -g _zpm_parallel_launcher
+typeset -g _zpm_parallel_item
+
+function _zpm_use_parallel_runner() {
+  _zpm_parallel_format='%s\n'
+  _zpm_parallel_launcher="parallel"
+  _zpm_parallel_item='{1}'
+}
+
+function _zpm_use_rush_runner() {
+  _zpm_parallel_format='%s\n'
+  _zpm_parallel_launcher="rush"
+  _zpm_parallel_item='"{}"'
+}
+
+function _zpm_use_xargs_runner() {
+  _zpm_parallel_format='%s\0'
+  _zpm_parallel_launcher="xargs -0 -P12 -I{}"
+  _zpm_parallel_item='{}'
+}
+
+if [[ -n "$ZPM_PARALLEL_RUNNER" ]]; then
+  if [[ "$ZPM_PARALLEL_RUNNER" = 'parallel' ]]; then
+    _zpm_use_parallel_runner
+  elif [[ "$ZPM_PARALLEL_RUNNER" = 'rush' ]]; then
+    _zpm_use_rush_runner
+  elif [[ "$ZPM_PARALLEL_RUNNER" = 'xargs' ]]; then
+    _zpm_use_xargs_runner
+  else
+    echo "Can't detect parallel runner"
+    return 0
+  fi
+else
+  if (( $+commands[parallel] )); then
+    _zpm_use_parallel_runner
+  elif (( $+commands[rush] )); then
+    _zpm_use_rush_runner
+  else
+    _zpm_use_xargs_runner
+  fi
+fi
+
 autoload -Uz \
   zpm                            \
   @zpm-add-autoload              \
