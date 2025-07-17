c8fdea9aaf13f783cc1da1e43ce8ac00d87d0e16
🏴‍☠️ <224304+skull-squadron@users.noreply.github.com>
2025-07-16 23:48:04 -0500

    $HOME/.local/state/zsh -> $HOME/.local/state/zpm
    This isn't owned by the zsh project, it's zpm. This is a better place.

--- zpm.zsh
+++ zpm.zsh
@@ -14,7 +14,7 @@ export PMSPEC="0fbs"
 0="${${(M)0:#/*}:-$PWD/$0}"
 export _ZPM_DIR="${0:h}"
 
-export ZSH_TMP_DIR="${ZSH_TMP_DIR:-${XDG_STATE_HOME:-$HOME/.local/state}/zsh}"
+export ZSH_TMP_DIR="${ZSH_TMP_DIR:-${XDG_STATE_HOME:-$HOME/.local/state}/zpm}"
 _ZPM_CACHE="${ZSH_TMP_DIR}/zpm-cache.zsh"
 _ZPM_CACHE_ASYNC="${ZSH_TMP_DIR}/zpm-cache-async.zsh"
 
